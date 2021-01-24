a058aab7296e1b0450cded5ecf9e9dcd574f6c59
Sasi <sasisugel@gmail.com>
2021-01-24 23:01:32 +0530

    MongoDB connected

--- todo_list_backend/server.js
+++ todo_list_backend/server.js
@@ -1,7 +1,17 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const app = require('./app')
 
 dotenv.config({path: './config.env'})
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false
+})
+  .then(con => console.log(`DB connection successful!!`))
+  .catch(err => console.log(`ERR: ${err}`))
 
 const port  = process.env.PORT || 7000
 
