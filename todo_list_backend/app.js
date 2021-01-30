11ecde7e4da4fd00c1ebdcfd1c9b375bebaedb58
Sasi <sasisugel@gmail.com>
2021-01-30 13:37:40 +0530

    added global error handler and url identifier

--- todo_list_backend/app.js
+++ todo_list_backend/app.js
@@ -18,8 +18,22 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRourter)
 app.use('/api/v1/users', userRourter)
 
-app.all('/', (req, res) => {
-	res.status(500).json({message: 'You are accessing wrong url!!!'})
+app.all('*', (req, res) => {
+	// res.status(500).json({message: 'You are accessing wrong url!!!'})
+
+	const err = new Error(`Can't find ${req.originalUrl} on this server!`)
+	err.status = 'fail',
+	err.statusCode = 404
+})
+
+app.use((err, req, res, next) => {
+	err.statusCode = err.statusCode || 500
+	err.status = err.status || 'error'
+
+	res.status(err.statusCode).json({
+		status: err.status,
+		message: err.message
+	})
 })
 
 module.exports = app
