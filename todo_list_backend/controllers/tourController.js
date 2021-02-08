c7c047dfff3982018f1a36d25e48925ee448a38d
Sasi <sasisugel@gmail.com>
2021-02-08 21:43:30 +0530

    joined two table while get using populate refactored

--- todo_list_backend/controllers/tourController.js
+++ todo_list_backend/controllers/tourController.js
@@ -25,10 +25,7 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 })
 
 exports.getTour = catchAsync(async (req, res, next) => {
-	const tour = await Tour.findById(req.params.id).populate({
-		path: 'guides',
-		select: '-__v, -passwordChangedAt'
-	})
+	const tour = await Tour.findById(req.params.id)
 
 	if (!tour) {
 		return next(new AppError('Your requested ID does not exist', 404))
